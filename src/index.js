f02954c66eb07e037d629dbdbe261bf43e8e5c32
Tom Penyikie <75012084+charistom77@users.noreply.github.com>
2023-09-19 09:10:18 +0000

    First commit after authentication

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+const supabase = createClient('<INSERT PROJECT URL>', '<INSERT PROJECT ANON API KEY>')
+
+const App = () => (
+  <Auth
+    supabaseClient={supabase}
+    appearance={{
+      theme: ThemeSupa,
+      variables: {
+        default: {
+          colors: {
+            brand: 'red',
+            brandAccent: 'darkred',
+          },
+        },
+      },
+    }}
+  />
+)
